d0cc74f78388a777aaebdd30e844caf588d28344
Sultan Akdogan <sultanakdogan026@gmail.com>
2022-12-05 21:12:27 +0000

    flask-01 and flask-02 added

---
+++ Flask/flask-01-02-hello-world-app-Jinja-Template/hello-world-app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+    
+@app.route('/')
+def hello():
+    return "Hello World from Flask!!!"   
+
+@app.route('/second')
+def second():
+    return 'Bize Her Yer Trabzon!!!!' 
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id number of this page is {id}'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=2000)
